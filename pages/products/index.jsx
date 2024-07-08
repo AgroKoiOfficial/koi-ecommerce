3d18f333c6cc4e373620de4dc40d542e18153938
agustrio1 <agustrio1998@gmail.com>
2024-07-08 08:05:11 +0700

    change

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -2,18 +2,13 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Head from "next/head";
 import ProductCard from "../../components/product/ProductCard";
-import { CTA } from "@/components/CTA";
-import dynamic from "next/dynamic";
-
-const GoogleAnalytics = dynamic(() => import('@next/third-parties/google').then(mod => mod.GoogleAnalytics), { ssr: false });
-
+import {CTA} from "@/components/CTA"; 
 
 export default function Products({ products, totalProducts }) {
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [allProducts, setAllProducts] = useState(products);
   const [isLoadingData, setIsLoadingData] = useState(true);
-  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
     setIsLoadingData(false);
@@ -40,19 +35,9 @@ export default function Products({ products, totalProducts }) {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}?page=${nextPage}`
     );
-
-    if (response.ok) {
-      const newData = await response.json();
-      setAllProducts([...allProducts, ...newData]);
-      setPage(nextPage);
-      if (newData.length === 0) {
-        setHasMore(false);
-      }
-    } else {
-      console.error("Failed to load more products");
-      setHasMore(false); 
-    }
-    
+    const newData = await response.json();
+    setAllProducts([...allProducts, ...newData]);
+    setPage(nextPage);
     setLoading(false);
   };
 
@@ -65,6 +50,8 @@ export default function Products({ products, totalProducts }) {
           content="Temukan berbagai produk ikan koi berkualitas di toko kami. Kami menyediakan ikan koi dengan harga terjangkau dan kualitas terbaik. Jelajahi koleksi kami sekarang!"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="icon" href="/logo.png" />
       </Head>
       <main className="pt-16 lg:pt-20 mb-16 lg:mb-20">
         <h1 className="text-3xl lg:text-4xl font-bold mb-8 text-center my-4">
@@ -72,7 +59,7 @@ export default function Products({ products, totalProducts }) {
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
           {allProducts.map((product, index) => (
-            <Link href={`/products/${product.slug}`} key={index}>
+            <Link href={`/products/${product.id}`} key={index}>
               <ProductCard
                 key={index}
                 product={product}
@@ -82,22 +69,17 @@ export default function Products({ products, totalProducts }) {
           ))}
         </div>
         <div className="flex justify-center mt-8">
-          {hasMore ? (
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={loadMore}
-              disabled={loading}
-            >
-              {loading ? "Loading..." : "Load More"}
-            </button>
-          ) : (
-            <p className="text-center mt-8">You've reached the end of the list</p>
-          )}
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={loadMore}
+            disabled={loading}
+          >
+            {loading ? "Loading..." : "Load More"}
+          </button>
         </div>
         <CTA />
+        {loading && <p className="text-center mt-8">Loading...</p>}
       </main>
-
-      <GoogleAnalytics gaId="G-BKXLWYCWM3" />
     </>
   );
 }
@@ -107,30 +89,13 @@ export async function getServerSideProps({ req, res }) {
     "Cache-Control",
     "public, s-maxage=60, stale-while-revalidate=120"
   );
-
-  try {
-    const products = await fetch(`${process.env.NEXT_PUBLIC_API_URL}?page=1`);
-    
-    if (!products.ok) {
-      throw new Error("Failed to fetch products");
-    }
-    
-    const productsData = await products.json();
-    const totalProducts = parseInt(products.headers.get("X-Total-Count"), 10);
-
-    return {
-      props: {
-        products: productsData,
-        totalProducts,
-      },
-    };
-  } catch (error) {
-    console.error("Error fetching products:", error);
-    return {
-      props: {
-        products: [],
-        totalProducts: 0,
-      },
-    };
-  }
+  const products = await fetch(`${process.env.NEXT_PUBLIC_API_URL}?page=1`);
+  const productsData = await products.json();
+  const totalProducts = parseInt(products.headers.get("X-Total-Count"));
+  return {
+    props: {
+      products: productsData,
+      totalProducts,
+    },
+  };
 }
