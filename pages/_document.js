b2da88a8f2a8dfe03de64c85d61d2e40d7fc81fe
agustrio1 <agustrio1998@gmail.com>
2024-07-08 15:51:23 +0700

    csp

--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,11 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default function Document() {
   const nonce = uuidv4();
+ const csp = `object-src 'none'; base-uri 'none'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https: http: 'nonce-${nonce}' 'strict-dynamic'`
 
   return (
-    <Html lang="en">
-      <Head>
+    <Html lang="id">
+      <Head  nonce={nonce}>
         <meta name="theme-color" content="#ffffff" />
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -15,20 +16,21 @@ export default function Document() {
         <meta name="author" content="Agro Koi" />
         <link rel="icon" href="/logo.png" />
         <link rel="apple-touch-icon" href="/logo.png" />
-        <meta property="og:title" content="Your Website Title" />
+        <meta property="og:title" content="Koi Toko" />
         <meta
           property="og:description"
-          content="Your website description here"
+          content="Kami menjual Ikan Koi Berkualitas"
         />
         <meta property="og:image" content="/logo.png" />
-        <meta property="og:url" content="https://yourwebsite.com" />
+        <meta property="og:url" content="https://trioagus.cloud" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Your Website Title" />
         <meta
           name="twitter:description"
-          content="Your website description here"
+          content="Kami menjual Ikan Koi Berkualitas"
         />
         <meta name="twitter:image" content="/logo.png" />
+        <meta httpEquiv="Content-Security-Policy" content={csp} />
       </Head>
       <body>
         <Main />
