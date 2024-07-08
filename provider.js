98249d463fe55503001b836539e2ef121d436627
agustrio1 <agustrio1998@gmail.com>
2024-07-08 10:05:34 +0700

    rendering footer

--- provider.js
+++ provider.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Navbar from "@/components/header/Navbar";
 import Footer from "@/components/Footer";
@@ -6,13 +6,19 @@ import Footer from "@/components/Footer";
 function Provider({ children }) {
   const { pathname } = useRouter();
   const disableNavbar = ['register', 'login', 'reset-password', 'forgot-password', 'dashboard', 'user'];
-  const disableFooter = ['register', 'login', 'reset-password', 'forgot-password',  'dashboard', 'user'];
+  const disableFooter = ['register', 'login', 'reset-password', 'forgot-password', 'dashboard', 'user'];
+
+  const [isContentLoaded, setIsContentLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsContentLoaded(true);
+  }, []);
 
   return (
     <>
       {!disableNavbar.includes(pathname.split('/')[1]) && <Navbar />}
       {children}
-      {!disableFooter.includes(pathname.split('/')[1]) && <Footer />}
+      {isContentLoaded && !disableFooter.includes(pathname.split('/')[1]) && <Footer />}
     </>
   );
 }
