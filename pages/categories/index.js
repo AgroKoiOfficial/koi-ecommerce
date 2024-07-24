5eac1be7c6457e4b41f3bc799962e2f59ac1a99a
agustrio1 <agustrio1998@gmail.com>
2024-07-24 09:24:57 +0700

    update category pages

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useTheme } from 'next-themes';
+import { useTheme } from "next-themes";
 import { formatRupiah } from "@/utils/currency";
 import { Input } from "@/components/ui/Input";
 import {
@@ -47,7 +47,7 @@ export default function Categories({ productsWithUrl, displayedCategories }) {
   const router = useRouter();
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [filteredProducts, setFilteredProducts] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const { theme } = useTheme();
 
   useEffect(() => {
@@ -112,17 +112,27 @@ export default function Categories({ productsWithUrl, displayedCategories }) {
                   <div
                     key={category}
                     className={`category-box p-1 rounded-md cursor-pointer ${
-                      selectedCategory === category ? "border-2 border-red-500" : ""
-                    } ${theme === "dark" ? "shadow-md shadow-gray-700" : "shadow-md shadow-gray-300"}`}
-                    onClick={() => handleCategorySelect(category)}
-                  >
+                      selectedCategory === category
+                        ? "border-2 border-red-500"
+                        : ""
+                    } ${
+                      theme === "dark"
+                        ? "shadow-md shadow-gray-700"
+                        : "shadow-md shadow-gray-300"
+                    }`}
+                    onClick={() => handleCategorySelect(category)}>
                     <h4 className="text-sm lg:text-lg text-center">
                       {category}
                     </h4>
                   </div>
                 ))
               ) : (
-                <p className={`text-center ${theme === "dark" ? "text-gray-300" : "text-gray-800"}`}>No categories found.</p>
+                <p
+                  className={`text-center ${
+                    theme === "dark" ? "text-gray-300" : "text-gray-800"
+                  }`}>
+                  No categories found.
+                </p>
               )}
             </div>
             <ScrollBar orientation="horizontal" />
@@ -130,16 +140,22 @@ export default function Categories({ productsWithUrl, displayedCategories }) {
         </div>
         {selectedCategory && (
           <div className="mt-8">
-            <h2 className={`text-2xl text-center font-bold mb-4 lg:mb-6 ${theme === "dark" ? "text-gray-300" : "text-gray-800"}`}>
+            <h2
+              className={`text-2xl text-center font-bold mb-4 lg:mb-6 ${
+                theme === "dark" ? "text-gray-300" : "text-gray-800"
+              }`}>
               Produk {selectedCategory}
             </h2>
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
               {filteredProducts.map((product) => (
                 <Link href={`/products/${product.slug}`} key={product.id}>
                   <Card
                     key={product.id}
-                    className={`p-4 rounded-md shadow-md ${theme === "dark" ? "text-gray-300 shadow-gray-700" : "text-gray-800 shadow-gray-300"}`}
-                  >
+                    className={`p-4 rounded-md shadow-md ${
+                      theme === "dark"
+                        ? "text-gray-300 shadow-gray-700"
+                        : "text-gray-800 shadow-gray-300"
+                    }`}>
                     <CardHeader>
                       <Image
                         src={product.image}
@@ -147,16 +163,20 @@ export default function Categories({ productsWithUrl, displayedCategories }) {
                         width={100}
                         height={100}
                         priority={true}
-                        style={{
-                          width: "auto",
-                          height: "auto",
-                          margin: "0 auto",
-                        }}
+                        style={{ objectFit: "contain" }}
+                        className="h-72 w-full mx-auto"
                       />
                     </CardHeader>
                     <CardContent className="mt-4 flex flex-col items-center space-y-1">
-                      <CardTitle className="text-sm md:text-md lg:text-lg font-semibold">{product.name}</CardTitle>
-                      <p className="text-[0.7rem] lg:text-md">Stok: {product.stock}</p>
+                      <CardTitle className="text-md lg:text-lg font-semibold">
+                        {product.name}
+                      </CardTitle>
+                      <p className="text-[.9rem] lg:text-md">
+                        Harga: {product.price}
+                      </p>
+                      <p className="text-[0.7rem] lg:text-md">
+                        Stok: {product.stock}
+                      </p>
                     </CardContent>
                   </Card>
                 </Link>
